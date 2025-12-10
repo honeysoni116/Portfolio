640c9a6a44b24dbf32c709aa7a1ac1abc39f15db
Honey soni <honey@izooto.com>
2025-12-10 12:54:33 +0530

    Update country sales ui

--- src/app/components/dashboard/components/country-sales/country-sales.ts
+++ src/app/components/dashboard/components/country-sales/country-sales.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 
 interface CountrySalesdata {
   name: string;
-  percentage: number; // 0-100%
+  percentage: number;
+  flagUrl: string;
 }
 
 
@@ -15,10 +16,10 @@ interface CountrySalesdata {
 })
 export class CountrySales {
   countries: CountrySalesdata[] = [
-    { name: 'USA', percentage: 92 },
-    { name: 'Poland', percentage: 68 },
-    { name: 'Germany', percentage: 62 },
-    { name: 'England', percentage: 78 },
-    { name: 'Netherland', percentage: 58 }
+    { name: 'USA', percentage: 92, flagUrl: 'https://flagcdn.com/w20/us.png' },
+    { name: 'Poland', percentage: 68, flagUrl: 'https://flagcdn.com/w20/pl.png' },
+    { name: 'Germany', percentage: 62, flagUrl: 'https://flagcdn.com/w20/de.png' },
+    { name: 'England', percentage: 78, flagUrl: 'https://flagcdn.com/w20/gb.png' },
+    { name: 'Netherland', percentage: 58, flagUrl: 'https://flagcdn.com/w20/nl.png' }
   ];
 }
